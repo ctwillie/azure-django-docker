cf64ddc7d95fb2a40c1337385a821acaaa9c81f1
Cedric Twillie <cedric.twillie@1nhealth.com>
2023-12-27 11:11:01 -0500

    chore: format code with black

--- blog/forms.py
+++ blog/forms.py
@@ -5,13 +5,13 @@ from .models import Blog
 
 class BlogForm(forms.ModelForm):
     def clean_title(self):
-        title = self.cleaned_data.get('title')
-        
+        title = self.cleaned_data.get("title")
+
         if len(title) < 4:
-            raise forms.ValidationError('Title must be at least 4 characters long')
-        
+            raise forms.ValidationError("Title must be at least 4 characters long")
+
         return title
-    
+
     class Meta:
         model = Blog
-        fields = ('title', 'body')
+        fields = ("title", "body")
--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .import views
+from . import views
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create', views.create, name='create'),
-    path('<int:pk>/update', views.update, name='update'),
-    path('<int:pk>/delete', views.delete, name='delete'),
+    path("", views.index, name="index"),
+    path("create", views.create, name="create"),
+    path("<int:pk>/update", views.update, name="update"),
+    path("<int:pk>/delete", views.delete, name="delete"),
 ]
